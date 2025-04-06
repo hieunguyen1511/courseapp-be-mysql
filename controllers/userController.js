dfd9a5fcbb277a445c11638773aff7904227f936
Linh Truong <thai@macbookair.local>
2025-04-06 21:42:39 +0700

    feat: add grantType to jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -272,22 +272,19 @@ async function login(req, res) {
 
     const access_token = jwt.sign(
       {
+        grantType: 'access_token',
         username: user.username,
         userId: user.id,
         role: user.role,
-        fullname: user.fullname,
-        email: user.email,
+      fullname: user.fullname,
+      email: user.email,
       },
       process.env.JWT_KEY,
       { expiresIn: '1h' },
     );
     const refresh_token = jwt.sign(
       {
-        username: user.username,
-        userId: user.id,
-        role: user.role,
-        fullname: user.fullname,
-        email: user.email,
+        grantType: 'refresh_token',
       },
       process.env.JWT_KEY,
       { expiresIn: '7d' },
@@ -541,7 +538,12 @@ async function refreshToken(req, res) {
           .status(401)
           .json({ message: 'Invalid or expired refresh token' });
       const access_token = jwt.sign(
-        { username: user.username, userId: user.id, role: user.role },
+        {
+          grantType: 'access_token',
+          username: user.username,
+          userId: user.id,
+          role: user.role,
+        },
         process.env.JWT_KEY,
         { expiresIn: '1h' },
       );
