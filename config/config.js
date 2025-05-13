469b330de681732a9ae3ffd6f39c1d3bf40233ce
Nguyen Trong Hieu <hieunguyentronghieu69@gmail.com>
2025-05-13 15:34:18 +0700

    feat: update config file structure and change config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: 'mysql',
+  },
+};
